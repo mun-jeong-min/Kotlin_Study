fc9fa752e90f7f9a63e0c27b2c16022ca51fbc40
1406MJM <3330kys@dsm.hs.kr>
2022-04-06 21:48:31 +0900

    [ADD] setOf, 함수에 return이 없을 경우 반환 타입: unit

--- Kotlin_Learning/src/Array.kt
+++ Kotlin_Learning/src/Array.kt
@@ -1,4 +1,13 @@
 fun main() {
     val strings = listOf("one", "two", "three")
     println(strings)
+
+    val strings2 = setOf("1","2","3")
+    println(strings2)
+
+    sum(4,5)
+}
+
+fun sum(a: Int, b: Int): Unit {
+    println("sum $a and sum $b")
 }
