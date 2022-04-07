53454c99d5e88abd583a7ca3709b503d3a683527
1406MJM <3330kys@dsm.hs.kr>
2022-04-07 11:56:41 +0900

    [EDIT] when을 이용한 실행 구문과 변수,상수

--- Kotlin_Learning/src/Package/Color.kt
+++ Kotlin_Learning/src/Package/Color.kt
@@ -6,6 +6,4 @@ public enum class Color(
     RED(255,0,0), ORANGE(255,165,0),
     YELLOW(255,255,0), GREEN(0,255,0),
     BLUE(0,0,255);
-
-    fun rgb() = (r * 256 + g) * 256 + b
 }
--- Kotlin_Learning/src/Package/funArray.kt
+++ Kotlin_Learning/src/Package/funArray.kt
@@ -8,11 +8,20 @@ fun s(color: Color) =
         Color.BLUE -> "B"
         Color.ORANGE -> "C"
         Color.YELLOW -> "D"
-        Color.GREEN -> "E"
+        Color.GREEN -> {
+            println("STARTTTTTT")
+        };
+        // Color.GREEN -> {
+        // 실행구문
+        // }
+        // 이런 형태로 실행 구문가능
     }
 
 fun main() {
+    var s:Int = 30 // var은 변할수 있는 변수
+    val r:Int = 20 // val은 변할수 없는 상수
+    var a = 30 // 타입 추론을 지원하기 때문에 자료형을 쓰지않아도 됨.
+    
     println(max(66,77))
-
-    println(s(Color.BLUE))
+    println(s(Color.GREEN))
 }
