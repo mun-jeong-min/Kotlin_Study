f85b675c351751042d9da52168f44d82e1896113
1406MJM <3330kys@dsm.hs.kr>
2022-04-08 23:58:19 +0900

    [ADD] array, lamda식

---
+++ Kotlin_Learning/src/Pack2/Stop.kt
@@ -0,0 +1,9 @@
+package Pack2
+
+fun main()
+{
+    val list:List<Int> = List(5,{i->i*2})
+
+    println(list)
+}
+
--- Kotlin_Learning/src/Package/funArray.kt
+++ Kotlin_Learning/src/Package/funArray.kt
@@ -21,6 +21,10 @@ fun main() {
     var s:Int = 30 // var은 변할수 있는 변수
     val r:Int = 20 // val은 변할수 없는 상수
     var a = 30 // 타입 추론을 지원하기 때문에 자료형을 쓰지않아도 됨.
+
+    var arr:IntArray = IntArray(40)
+
+    var sss:CharArray = CharArray(40)
     
     println(max(66,77))
     println(s(Color.GREEN))
