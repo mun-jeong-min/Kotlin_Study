539ec6f61ba814877fc9119df8e1ac476f8e7b32
munjeongmin <3330kys@dsm.hs.kr>
2022-07-28 11:51:19 +0900

    DELETE :: ignore files

--- Kotlin_Learning/src/Pack2/Stop.kt
+++
@@ -1,9 +0,0 @@
-package Pack2
-
-fun main()
-{
-    val list:List<Int> = List(5,{i->i*2})
-
-    println(list)
-}
-
--- Kotlin_Learning/src/Package/Color.kt
+++
@@ -1,9 +0,0 @@
-package Package
-
-public enum class Color(
-    val r:Int, val g:Int, val b:Int
-) {
-    RED(255,0,0), ORANGE(255,165,0),
-    YELLOW(255,255,0), GREEN(0,255,0),
-    BLUE(0,0,255);
-}
