9c55a53ea761e8120b61fadc3e3a0dc9799867d2
munjeongmin <3330kys@dsm.hs.kr>
2022-07-28 15:56:03 +0900

    ADD :: listof, setof

---
+++ kotlin_st/src/main/kotlin/array/Array.kt
@@ -0,0 +1,22 @@
+package main.kotlin.array
+
+fun main() {
+    val items = listOf("apple", "banana", "kiwi")
+    for(item in items) {
+        println(item)
+    }
+
+    val items2 = setOf("apple", "banana", "kiwi")
+    when {
+        "orange" in items2 -> print("melon")
+        "apple" in items2 -> print("watermelon")
+    }
+    println(items2)
+
+    items
+        .filter { it.length > 4 }
+        .sortedBy { it }
+        .map { it.toUpperCase() }
+        .forEach { println(it) }
+}
+
